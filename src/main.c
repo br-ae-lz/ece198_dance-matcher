e86cde9a9ccaaf6584d6cc5f33b658fd6c56b142
Braden <bradenschulz@gmail.com>
2021-11-21 19:57:16 -0500

    updated sensor and LED demo for final hardware rig now that hardware is all working fine, puzzle implementation is next

--- src/main.c
+++ src/main.c
@@ -1,78 +1,115 @@
-// Modified sample code for ECE 198
+// ECE 198 Pressure Sensor LED Pattern Project
+// Written by Braden Schulz, November 2021
 
-// Written by Bernie Roehl, August 2021
-// Modified by Braden Schulz, November 2021
-
-#include <stdbool.h> // booleans, i.e. true and false
-#include <stdio.h>   // sprintf() function
-#include <stdlib.h>  // srand() and random() functions
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h> 
 
 #include "ece198.h"
 
+// define minimum ADC values for each sensor that indicate they are being pressed
+#define A1_PRESSED 3000
+#define A2_PRESSED 3000
+#define A3_PRESSED 3000
+#define A4_PRESSED 3000
+
+void readSensors(ADC_HandleTypeDef adcInstance, uint16_t ADCPressed[]);
+
 int main(void)
 {
-    HAL_Init(); // initialize the Hardware Abstraction Layer
+    // initialize HAL and enable ports to be used
+    HAL_Init();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_ADC1_CLK_ENABLE();
 
-    __HAL_RCC_GPIOA_CLK_ENABLE(); // enable port A (for the on-board LED, for example)
-    __HAL_RCC_GPIOB_CLK_ENABLE(); // enable port B (for the rotary encoder inputs, for example)
-    __HAL_RCC_GPIOC_CLK_ENABLE(); // enable port C (for the on-board blue pushbutton, for example)
+    // initalize D12, D11, D10, and D9 for output
+    // (correspond to yellow, green, red, and blue LEDs respectively)
+    InitializePin(GPIOA, GPIO_PIN_6 | GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);
+    InitializePin(GPIOB, GPIO_PIN_6, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);
+    InitializePin(GPIOC, GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);
+    // initialize A1, A2, A3, and A4 for analog input
+    // (correspond to pressure sensors for yellow, green, red, and blue LEDs respectively)
+    InitializePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_4, GPIO_MODE_ANALOG, GPIO_NOPULL, 0);
+    InitializePin(GPIOB, GPIO_PIN_0, GPIO_MODE_ANALOG, GPIO_NOPULL, 0);   
+    InitializePin(GPIOC, GPIO_PIN_1, GPIO_MODE_ANALOG, GPIO_NOPULL, 0); 
 
-    InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);  // on-board LED
-
-
-    // prepare LED by setting up D12 for output
-    InitializePin(GPIOA, GPIO_PIN_6, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);
-
-    // set up for serial communication to the host computer
-    // (anything we write to the serial port will appear in the terminal (i.e. serial monitor) in VSCode)
-
+    // set up serial communication
     SerialSetup(9600);
 
-    __HAL_RCC_ADC1_CLK_ENABLE();        // enable ADC 1
-    ADC_HandleTypeDef adcInstance;      // this variable stores an instance of the ADC
-    InitializeADC(&adcInstance, ADC1);  // initialize the ADC instance
+    // declare and initialize ADC instance
+    ADC_HandleTypeDef adcInstance;
+    InitializeADC(&adcInstance, ADC1);
 
-    // initialize temporary values to store min and max readings and whether input is read
-    uint16_t maxraw0 = 0;
-    uint16_t minraw0 = 10000;
-    bool pressed = false;
+    // declare analog input array to store pressed state for pins A1-A4
+    uint16_t ADCPressed[4];
 
-    // Enables the input pins
-    // (on this board, pin A0 is connected to channel 0 of ADC1, and A1 is connected to channel 1 of ADC1)
-    InitializePin(GPIOA, GPIO_PIN_0 | GPIO_PIN_1, GPIO_MODE_ANALOG, GPIO_NOPULL, 0);   
     while (true)
     {
-        // read the ADC values (0 -> 0V, 2^12 -> 3.3V)
-        uint16_t raw0 = ReadADC(&adcInstance, ADC_CHANNEL_0);
+        readSensors(adcInstance, ADCPressed);
 
-        // update min and max values if readings lie outside these limits
-        if (maxraw0 < raw0) {
-            maxraw0 = raw0;
-        } if (minraw0 > raw0) {
-            minraw0 = raw0;
-        } 
-        
-        // reading above 3000 is read as a press, so print whether sensor is being pressed based off of that
-        if (raw0 > 2000) {
-            pressed = true;
+        // for as long as sensor is pressed, light up its corresponding LED
+        if (ADCPressed[0]) {
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
         } else {
-            pressed = false;
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
         }
 
-        // print the ADC values, min and max values read so far, and whether sensor is being pressed
+        if (ADCPressed[1]) {
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+        } else {
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+        }
+
+        if (ADCPressed[2]) {
+            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+        } else {
+            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+        }
+
+        if (ADCPressed[3]) {
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+        } else {
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+        }
+
+        // print the raw ADC values for pins A1-A4
         char buff[100];
-        sprintf(buff, "Channel0: %hu, max: %hu, min: %hu, pressed: %d\r\n", raw0, maxraw0, minraw0, pressed);  // hu == "unsigned short" (16 bit)
+        sprintf(buff, "Pin A1 (Y): %hu \t Pin A2 (G): %hu \t Pin A3 (R): %hu \t Pin A4 (B): %hu \n", 
+        ReadADC(&adcInstance, ADC_CHANNEL_1), ReadADC(&adcInstance, ADC_CHANNEL_4), ReadADC(&adcInstance, ADC_CHANNEL_8), ReadADC(&adcInstance, ADC_CHANNEL_11));
         SerialPuts(buff);
     }
 
     return 0;
 }
 
-// This function is called by the HAL once every millisecond
+// reads pressure sensors and updates their pressed states
+void readSensors(ADC_HandleTypeDef adcInstance, uint16_t ADCPressed[]) {
+    if (ReadADC(&adcInstance, ADC_CHANNEL_1) > A1_PRESSED) {
+        ADCPressed[0] = 1;
+    } else {
+        ADCPressed[0] = 0;
+    }
+    if (ReadADC(&adcInstance, ADC_CHANNEL_4) > A2_PRESSED) {
+        ADCPressed[1] = 1;
+    } else {
+        ADCPressed[1] = 0;
+    }
+    if (ReadADC(&adcInstance, ADC_CHANNEL_8) > A3_PRESSED) {
+        ADCPressed[2] = 1;
+    } else {
+        ADCPressed[2] = 0;
+    }
+    if (ReadADC(&adcInstance, ADC_CHANNEL_11) > A4_PRESSED) {
+        ADCPressed[3] = 1;
+    } else {
+        ADCPressed[3] = 0;
+    }
+}
+
+// called by the HAL once every millisecond
 void SysTick_Handler(void)
 {
-    HAL_IncTick(); // tell HAL that a new tick has happened
-    // we can do other things in here too if we need to, but be careful
+    HAL_IncTick();  // increment tick count
 }
